6a28406731bb1a3270f79319db12395669a9afde
David Hilzendegen <david.hilzendegen@posteo.de>
2017-10-23 11:28:51 +0200

    Update

--- js/map.js
+++ js/map.js
@@ -10,17 +10,6 @@ function setInitialZoom() {
     return initZoom;
 };
 
-/*function setInitialCenter() {
-    var viewportWidth = window.innerWidth;
-    var initCenter;
-    if (viewportWidth < [800]) {
-        initCenter = [49.1426930, 9.2108790]; //Heilbronn
-    } else {
-        initCenter = [48.6616040, 9.3501340]; //Baden-Württemberg
-    }
-    return initCenter;
-};*/
-
 // Die eigentliche Karte initialisieren
 function main() {
 
@@ -44,10 +33,14 @@ function main() {
         minZoom: 7,
         maxZoom: 17,
         zoomControl: false,
+        zoomDelta: .25,
+        zoomSnap: .25,
         attributionControl: true,
         legends: false,
         layer_selector: false,
         maxBounds: bounds,
+        //       dragging: false,
+        //      touchZoom: true
     };
 
     // Karte und Basemap initialisieren
@@ -131,11 +124,13 @@ function main() {
         pointToLayer: function(feature, latlng) {
             return L.marker(latlng, {
                 riseOnHover: true,
-                icon: yellowIcon,
+                icon: orangeIcon,
             });
         },
         onEachFeature: onEachFeatureZahn,
     });
+
+    var gemeinden = L.geoJson(gemeinden).addTo(map);
 
     // Layer mit Cluster erstellen
     hausarztMarker.addLayer(ha);
@@ -167,7 +162,7 @@ function main() {
 
     var zahnarztClick = function() {
         document.getElementById("infotext").innerHTML = "<center><p><b>Klicken Sie auf die Karte.</b></p></center>";
-        document.querySelector('.za').style.backgroundColor = "rgba(203, 197, 41, .6)";
+        document.querySelector('.za').style.backgroundColor = "rgba(203, 133, 43, .6)";
         document.querySelector('.ha').style.backgroundColor = "";
         document.querySelector('.fa').style.backgroundColor = "";
         copy = L.geoJSON(zahnaerzte_region);
@@ -230,10 +225,70 @@ function main() {
         textClassName: 'swoopy-arrow',
         minZoom: 4,
         maxZoom: 9,
-        factor: 0.75,
-        wight: 10,
+        weight: 1,
         iconAnchor: [25, 15]
     }).addTo(map);
+
+    new L.SwoopyArrow([49.1050590, 9.5682240], [49.2116813, 9.4723779], {
+        text: 'Öhringen',
+        color: '#000000',
+        textClassName: 'swoopy-arrow',
+        minZoom: 4,
+        maxZoom: 9,
+        weight: 1,
+        iconAnchor: [15, 0]
+    }).addTo(map);
+
+    new L.SwoopyArrow([49.2176650, 8.7068970], [49.1379830, 8.9092340], {
+        text: 'Eppingen',
+        color: '#000000',
+        textClassName: 'swoopy-arrow',
+        minZoom: 4,
+        maxZoom: 9,
+        weight: 1,
+        iconAnchor: [55, 10]
+    }).addTo(map);
+
+
+    const activate = document.querySelector('#activate-btn');
+    activate.addEventListener('click', () => {
+        if (enabled) {
+            activate.innerHTML = 'Karte aktivieren';
+            disableMap()
+        } else {
+            activate.innerHTML = 'Karte deaktivieren';
+            enableMap()
+        };
+        enabled = !enabled;
+    });
+
+
+    function disableMap() {
+        map.dragging.disable();
+        map.touchZoom.disable();
+        map.doubleClickZoom.disable();
+        map.scrollWheelZoom.disable();
+        map.boxZoom.disable();
+        map.keyboard.disable();
+        if (map.tap) map.tap.disable();
+        document.getElementById('map').style.cursor = 'default';
+        document.getElementById('activate-btn').style.backgroundColor = "rgb(247, 255, 0)";
+    }
+
+    function enableMap() {
+        map.dragging.enable();
+        map.touchZoom.enable();
+        map.doubleClickZoom.enable();
+        map.scrollWheelZoom.enable();
+        map.boxZoom.enable();
+        map.keyboard.enable();
+        if (map.tap) map.tap.enable();
+        document.getElementById('map').style.cursor = 'grab';
+        document.getElementById('activate-btn').style.backgroundColor = "rgba(255, 255, 255, .6)";
+    }
+
+    let enabled = false;
+    disableMap();
 
 }
 
